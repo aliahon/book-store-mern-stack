61b21e43ec08a4066d8e32981192bfb9803e2201
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-08-18 20:08:49 +0100

    SPA and Add react router dom

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRoute } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRoute>
     <App />
-  </StrictMode>,
+  </BrowserRoute>,
 )
