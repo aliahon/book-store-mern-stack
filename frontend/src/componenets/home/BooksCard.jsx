6168660ad8db0517377675447a930427cd421881
Nohaila EL IDRISSI <nohaila09el@gmail.com>
2024-08-31 23:46:25 +0100

    add book modal

--- frontend/src/componenets/home/BooksCard.jsx
+++ frontend/src/componenets/home/BooksCard.jsx
@@ -2,7 +2,7 @@ import BookSingleCard from "./BookSingleCard.jsx";
 
 const BooksCard = ({ books }) => {
   return (
-    <div className="grid sm:grid-col-2 lg:grid-col-3 xl:grid-col-4">
+    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {books.map((item) => (
         <BookSingleCard key={item._id} book={item} />
       ))}
